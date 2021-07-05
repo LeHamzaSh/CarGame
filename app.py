402179a4210ab29aa641d3418bd81b80adf04172
LeHamzaSh <hrsheikh05@gmail.com>
2021-07-05 20:24:32 +0400

    Second Commit
    1. Added while loop
    2. Added if conditions
    3. Convert all input into lowercase

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 #Car Game
+
+command = ""
+
+while command != "quit":
+    command = input("> ").lower()
+
+    if command == "start":
+        print("Car Started, Ready to roll!!")
+
+    elif command == "stop":
+        print("Car Stopped")
